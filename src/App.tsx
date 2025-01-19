11d6c53ba4bba3c0f81f29a9e51ea9feeb3c6c3c
Jayraj Ladkat <jayrajkladkat@gmail.com>
2025-01-20 01:51:36 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div>
+      <main className="relative">
+        Nav
+        <section className="xl:padding-1 wide:padding-r padding-b"></section>
+      </main>
+    </div>
+  )
+}
+
+export default App
